b4db0a3cbecb0c1a5328c1b9732f5db5dd559723
Kate Kolosovska-Shchudlo <kate.shchudlo@gmail.com>
2023-01-14 19:30:29 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const textInput = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+
+  if (event.target.value === "") {
+    output.textContent = "Anonymous";
+  }
+});
